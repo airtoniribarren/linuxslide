dd7ac665c3b4067b7083e08f7a7ed93c5f721388
Saymon Coppi <saymoncoppi@gmail.com>
2018-11-25 19:51:59 -0200

    Create flavor.sh

---
+++ base/etc/flavor.sh
@@ -0,0 +1,15 @@
+
+# edit /etc/issue
+echo "Linux Slide 0.1 \n \l" > /etc/issue
+
+# edit /etc/motd
+echo "" > /etc/issue
+
+# edit /etc/lsb-release
+echo 'DISTRIB_ID=Slide
+DISTRIB_RELEASE="0.1"
+DISTRIB_DESCRIPTION="Slide 0.1 "
+DISTRIB_CODENAME=unstable' > /etc/lsb-release
+
+# edit /etc/hostname
+echo "slide" > /etc/hostname
