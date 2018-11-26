bc7a2f585b6e6f8d8d3cfc3c2e45db11a49af6eb
Saymon Coppi <saymoncoppi@gmail.com>
2018-11-26 14:47:18 -0200

    Update install_xorg.sh

--- base/install_xorg.sh
+++ base/install_xorg.sh
@@ -12,7 +12,27 @@ function has_command() {
 	if [ "$UID" -eq "$ROOT_UID" ]; then
   
   clear
-  apt-get install --no-install-recommends xserver-xorg-core xserver-xorg-legacy xserver-xorg-video-vesa xserver-xorg-video-vmware xserver-xorg-input-all xinit xfonts-base x11-utils libxcursor1 libdrm-intel1 libgl1-mesa-dri libglu1-mesa x11-xserver-utils
+  apt-get install --no-install-recommends \
+  xserver-xorg-core \
+  xserver-xorg-legacy \
+  xserver-xorg-video-vesa \
+  xserver-xorg-video-vmware \
+  xserver-xorg-input-all \
+  xinit\ 
+  xfonts-base \
+  x11-utils \
+  libxcursor1 \
+  libdrm-intel1 \
+  libgl1-mesa-dri \
+  libglu1-mesa
+  
+  # install x11 server utils, apt-get would add cpp dependency, bullshit!
+  cd /tmp
+  apt-get download x11-xserver-utils
+  dpkg -x x11-xserver-utils*.deb /tmp/x11utils
+  cd /tmp/x11utils
+  cp -aR * /
+  
  
 	else
 		# Message  
