369c5dde0e8b3a863380e9939ddfe5ede8d691c6
Saymon Coppi <saymoncoppi@gmail.com>
2018-11-26 12:53:03 -0200

    Delete update_apt.sh

--- base/etc/update_apt.sh
+++
@@ -1,5 +0,0 @@
-#add non-free packages
-deb http://ftp.us.debian.org/debian stretch main contrib non-free
-deb http://security.debian.org/ testing/updates main contrib non-free
-#https://backports.debian.org/Instructions/
-deb http://ftp.debian.org/debian stretch-backports main
