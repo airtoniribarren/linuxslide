febb46fca3c218a47447b42c4d5635cd836315d5
Saymon Coppi <saymoncoppi@gmail.com>
2018-11-26 13:33:27 -0200

    Create install_desktop.sh

---
+++ base/x/install_desktop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ROOT_UID=0
+
+# check command avalibility
+function has_command() {
+    command -v $1 > /dev/null
+}
+# Load common properties and functions in the current script.
+. ./common.sh
+
+	if [ "$UID" -eq "$ROOT_UID" ]; then
+		clear
+    apt-get install --yes --no-install-recommends openbox compton tint2 feh scrot xterm libnotify4
+else
+		# Message  
+		echo -e "Please run this script as root..."
+	fi
