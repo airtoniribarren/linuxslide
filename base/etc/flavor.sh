ba5432b1445f31bf0577467e58c2596ec607040b
Saymon Coppi <saymoncoppi@gmail.com>
2018-11-26 10:45:48 -0200

    Update flavor.sh

--- base/etc/flavor.sh
+++ base/etc/flavor.sh
@@ -1,5 +1,5 @@
 # edit /etc/issue
-echo "Linux Slide 0.1 \n \l" | sudo tee /etc/issue
+echo "Linux Slide 0.1 \n \l" | tee /etc/issue
 
 # edit /etc/motd
 echo "" | sudo tee /etc/issue
@@ -8,7 +8,7 @@ echo "" | sudo tee /etc/issue
 echo 'DISTRIB_ID=Slide
 DISTRIB_RELEASE="0.1"
 DISTRIB_DESCRIPTION="Slide 0.1 "
-DISTRIB_CODENAME=unstable' | sudo tee  /etc/lsb-release
+DISTRIB_CODENAME=unstable' | tee  /etc/lsb-release
 
 # edit /etc/hostname
-echo "slide" | sudo tee /etc/hostname
+echo "slide" | tee /etc/hostname
